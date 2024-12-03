256aefcb2e2fb643ae5244eddff7e633078b5028
Paola Schmidt <shaymera123@gmail.com>
2024-12-03 21:10:19 +0100

    Day 3

---
+++ AOC_3_1/AOC_3_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+void main()
+{
+    string filePath = @"C:\Users\paola\advent_of_code\AOC_3_1\AOC_3_1\AOC_3_1.txt";
+
+    if (File.Exists(filePath))
+    {
+        string input = File.ReadAllText(filePath);
+        string pattern = @"mul\((\d+),(\d+)\)";
+
+        int result = 0;
+
+        foreach (Match match in Regex.Matches(input, pattern))
+        {
+            result += multiply(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
+        }
+        Console.WriteLine(result);
+    }
+}
+
+int multiply(int a, int b) { return a * b; }
+
+main();
---
+++ AOC_3_2/AOC_3_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+void main()
+{
+    string filePath = @"C:\Users\paola\advent_of_code\AOC_3_2\AOC_3_2\AOC_3_2.txt";
+
+    if (File.Exists(filePath))
+    {
+        string input = File.ReadAllText(filePath);
+        
+        string pattern = @"don't\(\)|do\(\)|mul\((\d+),(\d+)\)";
+
+        int result = 0;
+        bool enabled = true;
+
+        foreach (Match match in Regex.Matches(input, pattern))
+        {
+            if (match.Groups[0].Value == "do()")
+            {
+                enabled = true;
+            }
+            else if (match.Groups[0].Value == "don't()")
+            {
+                enabled = false;
+            }
+            else if (enabled)
+            {
+                result += multiply(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
+
+int multiply(int a, int b) { return a * b; }
+
+main();
+
+//so we can check for the indexes of the don't and do... then we can basically split the strings between a do and a dont and only apply the regex on those right? -> snip them out of the string
