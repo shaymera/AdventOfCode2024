4f285eb89b1fa5370ff5b29be6da9ad3a18f1b2f
Paola Schmidt <shaymera123@gmail.com>
2024-12-07 19:32:28 +0100

    day 6

---
+++ AOC_6_1/AOC_6_1/Program.cs
@@ -0,0 +1,103 @@
+﻿void main()
+{
+    string filePath = @"C:\Users\paola\advent_of_code\AOC_6_1\AOC_6_1\AOC_6_1.txt";
+
+    if (File.Exists(filePath))
+    {
+        string[] input = File.ReadAllLines(filePath);
+        List<List<char>> map = new List<List<char>>();
+        foreach (string line in input)
+        {
+            map.Add(line.ToList());
+        }
+
+        List<List<int>> visited = new List<List<int>>();
+
+        for (int i = 0; i < map.Count; i++)
+        {
+            for (int j = 0; j < map[i].Count; j++)
+            {
+                if (map[i][j] == '^')
+                {
+                    visited.Add(new List<int> { i, j });
+                }
+            }
+        }
+
+        List<List<int>> directions = new List<List<int>>()
+        {
+            new List<int>(){-1, 0},
+            new List<int>(){0, 1},
+            new List<int>(){1, 0},
+            new List<int>(){0, -1}
+        };
+
+        visited = moveGuard(map, visited, directions);
+        
+        int result = visited.Count;
+
+        Console.WriteLine(result);
+    }
+}
+
+List<List<int>> moveGuard(List<List<char>> map, List<List<int>> visited, List<List<int>> directions) 
+{
+    bool guardPresent = true;
+    int directionIndex = 0; 
+    int locationIndex = 0;
+    while (guardPresent)
+    {
+        int tmpY = visited[locationIndex][0] + directions[directionIndex][0];
+        int tmpX = visited[locationIndex][1] + directions[directionIndex][1];
+
+        if (Enumerable.Range(0, map.Count).Contains(tmpY) && Enumerable.Range(0, map[0].Count).Contains(tmpX)) 
+        {
+            if (map[tmpY][tmpX] == '#') 
+            {
+                directionIndex = changeDirection(directions, directionIndex);
+            } 
+            else 
+            {
+                int tmpIndex = findIndex(visited, tmpY, tmpX);
+                if (tmpIndex != -1) 
+                {
+                    locationIndex = tmpIndex;
+                } 
+                else 
+                {
+                    locationIndex = visited.Count; 
+                    visited.Add(new List<int>() { tmpY, tmpX});
+                }
+            }
+        }
+        else { guardPresent = false; }
+    }
+    return visited; 
+}
+
+int findIndex(List<List<int>> visited, int y, int x)
+{
+    for (int i = 0; i < visited.Count; i++)
+    {
+        if (visited[i][0] == y && visited[i][1] == x)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int changeDirection(List<List<int>> directions, int currentDirection)
+{
+    if (currentDirection == (directions.Count-1))
+    {
+        return 0;
+    }
+    else
+    {
+        currentDirection++;
+        return currentDirection;
+    }
+}
+
+main();
---
+++ AOC_6_2/AOC_6_2/Program.cs
@@ -0,0 +1,117 @@
+﻿void main()
+{
+    string filePath = @"C:\Users\paola\advent_of_code\AOC_6_2\AOC_6_2\AOC_6_2.txt";
+
+    if (File.Exists(filePath))
+    {
+        var input = File.ReadAllLines(filePath).ToList();
+
+        //Count all rows & columns 
+        int rowCount = input.Count();
+        int columnCount = input[0].Count();
+
+        var barrels = new HashSet<(int row, int col)>();
+        (int row, int col) guardPosition = (0, 0);
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < columnCount; j++)
+            {
+                if (input[i][j] == '#')
+                {
+                    barrels.Add((i, j));
+                }
+                else if (input[i][j] == '^')
+                {
+                    guardPosition = (i, j);
+                }
+            }
+        }
+
+        var directions = new (int dRow, int dCol)[]
+        {
+            (-1, 0),
+            (0, 1),
+            (1, 0),
+            (0, -1)
+        };
+
+        var startPosition = (guardPosition.row, guardPosition.col, 0);
+        var (_, initialPath) = simulatePatrol(barrels, startPosition, rowCount, columnCount, directions);
+
+        // Count the number of positions that cause a loop when a wall is added
+        var uniqueVisitedPositions = initialPath
+            .Select(pos => (pos.row, pos.col))
+            .Distinct();
+
+        int loopCount = 0;
+        
+        foreach(var pos in uniqueVisitedPositions)
+        {
+            if (pos == guardPosition)
+            {
+                continue;
+            }
+            var testBarrel = new HashSet<(int row, int col)>(barrels) { pos };
+            var (foundLoop, _) = simulatePatrol(testBarrel, startPosition, rowCount, columnCount, directions);
+
+            if (foundLoop)
+            {
+                loopCount++;
+            }
+        }
+
+        Console.WriteLine(loopCount);
+    }
+}
+
+
+
+int changeDirection(int direction)
+{
+    return (direction + 1) % 4;
+}
+
+bool IsInBounds(int row, int col, int rowCount, int columnCount)
+{
+    return row >= 0 && row < rowCount && col >= 0 && col < columnCount;
+}
+
+(int row, int col, int direction) moveGuard(HashSet<(int row, int col)> barrels, (int row, int col, int direction) position, (int dRow, int dCol)[] directions)
+{
+    var (currentRow, currentCol, currentDir) = position;
+    var (dRow, dCol) = directions[currentDir];
+    var newPosition = (currentRow + dRow, currentCol + dCol);
+
+    if (barrels.Contains(newPosition))
+    {
+        return (currentRow, currentCol, changeDirection(currentDir));
+    }
+    else
+    {
+        return (currentRow + dRow, currentCol + dCol, currentDir);
+    }
+}
+
+(bool foundLoop, HashSet<(int row, int col, int direction)> path) simulatePatrol 
+    (HashSet<(int row, int col)> barrels, (int row, int col, int direction) initialPosition, int rowCount, int columnCount, (int dRow, int dCol)[] directions)
+{
+    var visited = new HashSet<(int row, int col, int direction)>();
+    var (currentRow, currentCol, currentDir) = initialPosition;
+
+    while (true)
+    {
+        if (!IsInBounds(currentRow, currentCol, rowCount, columnCount))
+        {
+            return (false, visited);
+        } 
+        else if (visited.Contains((currentRow, currentCol, currentDir))) 
+        {
+            return (true, visited);
+        }
+
+        visited.Add((currentRow, currentCol, currentDir));
+        (currentRow, currentCol, currentDir) = moveGuard(barrels, (currentRow, currentCol, currentDir), directions);
+    }
+}
+
+main();
